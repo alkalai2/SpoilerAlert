bece3ab25f0380d8cece337f2e14fe1031a5d2a4
Neeraj Asthana <neeasthana@gmail.com>
2014-11-08 20:04:56 -0600

    adding facebook_blocker.js file that will be run each time facebook is openned by the user

---
+++ chrome_ext/facebook_blocker.js
@@ -0,0 +1,9 @@
+/**
+ * This class block all feeds on Facebook that the user specifies.
+ * 
+ * Steps:
+ * 	- get html from facebook
+ *	- order all posts
+ * 	- search for specified blocked posts
+ *	- run javascript to block all of those specified elements
+ */
