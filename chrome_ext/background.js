cadb787eaf3d6fc2e2fd99b884138ebbb42efd3f
Neeraj <neeasthana@gmail.com>
2014-11-11 09:51:03 -0600

    background.js will display a console message when it reaches the facebook site

--- chrome_ext/background.js
+++ chrome_ext/background.js
@@ -1,16 +1,9 @@
 /**
  * Holds the background process for the extension
  * Main logic for the extension exists here
+ */
 
-
-function click(event){
-	console.log('got inside of the background script');
-	console.log('Turning ' + tab.url + ' red!');
-  	chrome.tabs.executeScript({
-    	code: 'document.body.style.backgroundColor="red"'
-  	});
-}
-
-chrome.browserAction.onClicked.addListener(click);*/
-
-console.log('got here');
+chrome.tabs.onUpdated.addListener(function(id, info, tab){
+	console.log('got inside of the listener');
+	console.log(info);
+});
