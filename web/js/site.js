5de8227a1f65ce5e66ba5207b2af9db03519f9dc
Jonathan Alkalai <Alkalai@wirelessprvnat-172-17-9-5.near.illinois.edu>
2014-11-13 22:21:00 -0600

    improved css, added more features

--- web/js/site.js
+++ web/js/site.js
@@ -1,3 +1,37 @@
 /*
     File to hold Javascript for SpoilerAlert site
 */
+$(function(){
+
+	// $("#myform").submit(function(event){
+	// 	var thehtml = '<li style = "display:none"> ' + $("input:first").val + '<span class="ui-icon ui-icon-closethick"></span> </li>';
+	// 	$(thehmtml).prependTo("#termBank").fadeIn('slow');
+	// 	$(this).val("");
+	// 	event.preventDefault();
+	// })
+
+	$("#autocomplete2").focus();
+
+	// if($(".aligned").length==0){
+	// 	console.log("there exists an empty list..")
+	// 	$(".package").append('<li class = "listHolder"> NO TERMS ADDED </li>');
+	// }
+
+	function FooBar(){
+		alert("FOOBAR");
+	}
+	$( "#myform" ).submit(function( event ) {
+		var term = $("input:first").val();
+    	var thehtml = '<li class = "aligned"style = "display:none"> ' + term + '<span class="inline ui-icon ui-icon-closethick" onclick="FooBar()"></span> </li>';
+		$(thehtml).prependTo("#termBank").fadeIn('slow');
+		//$(this).("input").val("");
+		$("#autocomplete2").val("");
+
+		$(".listHolder").remove();
+		event.preventDefault();
+    		return;
+  				
+	});
+
+	 $("text:last").css("fill","#f2f2f2");
+})
